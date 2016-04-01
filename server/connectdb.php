53b1aa7199b8f5579ed8fcf00c3b469d87a67dad
Erik Genaro Rodriguez Lazcano <ErikRodriguez@eriks-MacBook-Pro.local>
2016-04-01 12:48:17 -0600

    cambios con pagina funcionado solo falta modificar los mailings

--- server/connectdb.php
+++ server/connectdb.php
@@ -1,7 +1,7 @@
 <?php 
 
-	$db = mysqli_connect('localhost', '55eabdbec82b', '55eabdbec82bconcepth', 'ExitoBD'); //---- DB Beta ----
-	//$db = mysqli_connect('localhost', 'root', 'root', 'ExitoBD'); --- DB Prueba --- 
+	//$db = mysqli_connect('localhost', '55eabdbec82b', '55eabdbec82bconcepth', 'ExitoBD'); //---- DB Beta ----
+	$db = mysqli_connect('localhost', 'root', 'root', 'ExitoBD');
 	if(mysqli_connect_errno())
 	{
 		echo 'Failed to connect to MySQL: '.mysqli_connect_error();
